1fd20990838d3b56781db68c7e8a79ee23077aa0
WizardCody <hoho3d@gmail.com>
2020-06-03 20:50:46 +0200

    Rename

--- SharedResources/FormProgress.Designer.cs
+++ SharedResources/FormProgress.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ExcelAddIn
+﻿namespace SharedResources
 {
     partial class FormProgress
     {
--- SharedResources/FormProgress.cs
+++ SharedResources/FormProgress.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ExcelAddIn
+namespace SharedResources
 {
     public partial class FormProgress : Form
     {
